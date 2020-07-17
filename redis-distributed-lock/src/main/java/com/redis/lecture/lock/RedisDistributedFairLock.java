642879ba6b9d13bcd0f46d09a2a40c6d5fa6853a
wangkun <wangkun2@tuniu.com>
2020-07-17 17:03:15 +0800

    add

--- redis-distributed-lock/src/main/java/com/redis/lecture/lock/RedisDistributedFairLock.java
+++ redis-distributed-lock/src/main/java/com/redis/lecture/lock/RedisDistributedFairLock.java
@@ -36,6 +36,8 @@ public class RedisDistributedFairLock {
 
     private static final String PREFIX_HASH_NAME = "distributed_lock_timeout:";
 
+    private static final String PREFIX_ZSET_NAME = "distributed_lock_timeout:";
+
     /**
      * 每次启动生成的一个标识
      */
@@ -234,6 +236,12 @@ public class RedisDistributedFairLock {
     }
 
 
+    /**
+     * 使用list加过期时间实现
+     * @param key
+     * @param time
+     * @return
+     */
     public boolean lock1(String key, long time) {
         String listKey = PREFIX_LIST_QUEUE_NAME + key;
         try {
@@ -267,6 +275,79 @@ public class RedisDistributedFairLock {
                     Long result = redisTemplate.execute(new DefaultRedisScript<>(lua, Long.class),
                             Arrays.asList(listKey, key), value, String.valueOf(expireTime));
                     if (result != null && (result == 0 || result == 1)) {
+                        return true;
+                    }
+                    TimeUnit.MILLISECONDS.sleep(200);
+                } catch (Exception e) {
+                    LOGGER.error("while loop error.", e);
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("lock error.", e);
+            return true;
+        }
+    }
+
+
+    public boolean lock2(String key, long time) {
+        String listKey = PREFIX_LIST_QUEUE_NAME + key;
+        String zSetKey = PREFIX_ZSET_NAME + key;
+        String prefix = uuid.toString();
+        try {
+            String threadId = String.valueOf(Thread.currentThread().getId());
+            String value = prefix + ":" + key + ":" + threadId + ":" + UUID.randomUUID().toString();
+            redisTemplate.execute(new DefaultRedisScript<>(
+                    "local list = redis.call('lrange', KEYS[1], 0, -1);" +
+                            "for index,value in pairs(list) do local prefix = ARGV[4];" +
+                            "if (string.sub(value, 0, #prefix) ~= prefix) then " +
+                            "redis.call('LREM', KEYS[1], 0, value);" +
+                            "redis.call('ZREM', KEYS[2], value);" +
+                            "end;" +
+                            "end;" +
+                    "redis.call('rpush', KEYS[1], ARGV[1]);" +
+                    "local listlength = redis.call('llen', KEYS[1]);" +
+                    "redis.call('zadd', KEYS[2], ARGV[2] + ARGV[3]*listlength, ARGV[1]);" +
+                    //"local expire = tonumber(ARGV[2]) * tonumber(listlength);" +
+                    //"redis.call('expire', KEYS[1], expire);" +
+                    //"redis.call('expire', KEYS[2], expire);" +
+                    "return listlength;", Long.class), Arrays.asList(listKey, zSetKey), value, String.valueOf(System.currentTimeMillis()), String.valueOf(TimeUnit.SECONDS.toMillis(time)), prefix);
+            //System.out.println("value = " + value + " ,size = " + size);
+            while (true) {
+                /**
+                 * 0表示list为空
+                 * 1表示zset没找到对应score或score过期
+                 * 2表示获取锁成功
+                 * 3表示获取锁失败
+                 * 4表示value和list中的第一个值不等，需要继续循环
+                 */
+                String lua = "local listFirstValue = redis.call('lindex', KEYS[1], 0);" +
+                        "if (listFirstValue == false) then " +
+                        "redis.call('del', KEYS[2]);" +
+                        "return 0;" +
+                        "end;" +
+                        "local expire = redis.call('ZSCORE', KEYS[2], listFirstValue);" +
+                        //"redis.log(redis.LOG_NOTICE, 'expire = '..expire..'=='..ARGV[1]);" +
+                        "if (expire == false or expire < ARGV[1]) then " +
+                        "redis.call('lpop', KEYS[1]);" +
+                        "redis.call('zrem', KEYS[2], listFirstValue);" +
+                        "return 1;" +
+                        "end;" +
+                        "if (listFirstValue == ARGV[2]) then " +
+                        "local result = redis.call('set', KEYS[3], ARGV[2], 'ex', ARGV[3], 'nx');" +
+                        "if (result ~= false) then " +
+                        "redis.call('lpop', KEYS[1]);" +
+                        "redis.call('zrem', KEYS[2], listFirstValue);" +
+                        "return 2;" +
+                        "else return 3;" +
+                        "end;" +
+                        "else return 4;" +
+                        "end;";
+                try {
+                    Long result = redisTemplate.execute(new DefaultRedisScript<>(lua, Long.class),
+                            Arrays.asList(listKey, zSetKey, key), String.valueOf(System.currentTimeMillis()), value, String.valueOf(TimeUnit.SECONDS.toSeconds(time)));
+                    //System.out.println(result);
+                    if (result != null && (result == 0 || result == 1 || result == 2)) {
+                        System.out.println(result);
                         return true;
                     }
                     TimeUnit.MILLISECONDS.sleep(200);
--- redis-distributed-lock/src/test/java/com/redis/lecture/lock/RedisDistributedFairLockTest.java
+++ redis-distributed-lock/src/test/java/com/redis/lecture/lock/RedisDistributedFairLockTest.java
@@ -127,4 +127,38 @@ public class RedisDistributedFairLockTest extends BaseTest {
         });
         threadPoolExecutor.awaitTermination(5, TimeUnit.MINUTES);
     }
+
+    @Test
+    public void lock2Test() throws InterruptedException {
+        IntStream.range(0, 1).parallel().forEach(value1 -> {
+            IntStream.range(0, 100).parallel().forEach(value -> {
+                threadPoolExecutor.execute(() -> {
+                    MDC.put(TracerUtils.TRACE_ID, TraceIdGenerator.generate());
+                    String key = "test" + value1;
+                    //LOGGER.info(key + ":" + Thread.currentThread().getId() + ",开始执行时间:" + LocalDateTime.now());
+                    //System.out.println(key + ":" + Thread.currentThread().getId() + ",开始执行时间:" + LocalDateTime.now());
+                    boolean result = fairLock.lock2(key, 2);
+                    //System.out.println(result);
+                    if (result) {
+                        //System.out.println(key + " 获取锁成功,时间:" + LocalDateTime.now());
+                        LOGGER.info(key + " 获取锁成功,时间:" + LocalDateTime.now());
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (Exception ignore) {
+
+                        } finally {
+                            //fairLock1.unlock(key);
+                            //System.out.println(key + " 释放锁成功时间:" + LocalDateTime.now());
+                            LOGGER.info(key + " 释放锁成功时间:" + LocalDateTime.now());
+                        }
+                    } else {
+                        //System.err.println(key + " 获取锁失败,时间:" + LocalDateTime.now());
+                        LOGGER.error(key + " 获取锁失败,时间:" + LocalDateTime.now());
+                    }
+                    MDC.remove(TracerUtils.TRACE_ID);
+                });
+            });
+        });
+        threadPoolExecutor.awaitTermination(10, TimeUnit.MINUTES);
+    }
 }
